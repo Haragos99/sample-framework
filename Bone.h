7522ee235360d026832c242deae7b5777ac4afae
Haragos99 <geri.haragos@gmail.com>
2024-04-08 16:36:25 +0200

    Separete Joint

--- Bone.h
+++ Bone.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <QGLViewer/qglviewer.h>
 #include "Matrix4.h"
 #include <Eigen/Eigen>
 #include "Mesh.h"
+#include "Joint.h"
 
 using qglviewer::Vec;
 using MyMesh = OpenMesh::TriMesh_ArrayKernelT<MyTraits>;
@@ -13,22 +13,6 @@ using Vector = OpenMesh::VectorT<double, 3>;
 
 // Probáljuk majd meg hogy csak a kiválasztot csont részen csináljuk a key framet.
 
-struct Keyframe {
-    Keyframe(float time, int index,const Vec& angles)
-        : time_(time), index_of_join(index),angles_(angles) {}
-
-    float time() const { return time_; }
-    const Vec& position() const { return position_; }
-    const Vec& angeles() const { return angles_; }
-    Vec angles_;
-    Vec position_;
-    Vec selected_point;
-    int index_of_join;
-private:
-    float time_;
-    
-    
-};
 
 struct Axes {
     bool shown;
@@ -43,45 +27,7 @@ struct Axes {
 
 
 
-struct Joint {
-    int id;// id of the join and the index of the position
-    std::vector<int> bones_id;
-    Vec point;
-    Vec Tpose;
-    Joint* parent;
-    Vec angel;
-    Vec pivot;
-    std::vector<Joint*> children;
-    std::vector<Keyframe> keyframes;
-    Joint(){}
-    Joint(Vec p, int _id) { point = p; id = _id; Tpose = p; parent = nullptr; }
-    Mat4 M;
-    Mat4 R;
-    Joint* searchbyid(Joint* j, int key);
-    void drawarrow(Joint* j);
 
-    void draw(Joint* j);
-    
-    void change_all_position(Joint* j, Vec dif);
-
-    void change_all_rotason(Joint* j, Vec pivot, Vec angles);
-
-    void set_deafult_matrix(Joint* j);
-
-    void addframe(Joint* j, Keyframe& frame);
-
-    void animaterotaion(Joint* j, float current_time, Mat4 M);
-
-    void reset_all(Joint* j);
-
-    void transform_point(Joint* j);
-
-    Mat4 getMatrix();
-
-    void calculateMatrecies(Joint* j, Vec pivot, Vec angles);
-
-
-};
 
 struct Bone {
     std::vector<int> joins_id;
---
+++ Joint.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <QGLViewer/qglviewer.h>
+#include "Matrix4.h"
+using qglviewer::Vec;
+
+
+struct Keyframe {
+    Keyframe(float time, int index, const Vec& angles)
+        : time_(time), index_of_join(index), angles_(angles) {}
+
+    float time() const { return time_; }
+    const Vec& position() const { return position_; }
+    const Vec& angeles() const { return angles_; }
+    Vec angles_;
+    Vec position_;
+    Vec selected_point;
+    int index_of_join;
+private:
+    float time_;
+
+
+};
+
+
+
+struct Joint {
+    int id;// id of the join and the index of the position
+    std::vector<int> bones_id;
+    Vec point;
+    Vec Tpose;
+    Joint* parent;
+    Vec angel;
+    Vec pivot;
+    std::vector<Joint*> children;
+    std::vector<Keyframe> keyframes;
+    Joint() {}
+    Joint(Vec p, int _id) { point = p; id = _id; Tpose = p; parent = nullptr; }
+    Mat4 M;
+    Mat4 R;
+    Joint* searchbyid(Joint* j, int key);
+    void drawarrow(Joint* j);
+
+    void draw(Joint* j);
+
+    void change_all_position(Joint* j, Vec dif);
+
+    void change_all_rotason(Joint* j, Vec pivot, Vec angles);
+
+    void set_deafult_matrix(Joint* j);
+
+    void addframe(Joint* j, Keyframe& frame);
+
+    void animaterotaion(Joint* j, float current_time, Mat4 M);
+
+    void reset_all(Joint* j);
+
+    void transform_point(Joint* j);
+
+    Mat4 getMatrix();
+
+    void calculateMatrecies(Joint* j, Vec pivot, Vec angles);
+
+
+};
+
